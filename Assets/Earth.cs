91720dd6ca39cb0e01fa873b32bbf861f596e129
Pei4 <40812695+Pei4@users.noreply.github.com>
2018-10-09 19:51:12 +0800

    Big Bang

---
+++ Assets/Earth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Earth : MonoBehaviour {
+    public float self;
+    public float around;
+    private Sun SunPos;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform transform = GetComponent<Transform>();
+        SunPos = GameObject.FindObjectOfType<Sun>();
+        Vector3 angle = new Vector3(0, -self*Time.deltaTime, 0);
+        Vector3 revolution = new Vector3(0, -around*Time.deltaTime,0);
+        transform.Rotate(angle);
+        transform.RotateAround(SunPos.transform.position,revolution,around);
+
+    }
+}
---
+++ Assets/Moon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moon : MonoBehaviour {
+    public float y;
+    public float around;
+    private Earth EarthPos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform transform = GetComponent<Transform>();
+        EarthPos = GameObject.FindObjectOfType<Earth>();
+        Vector3 angle = new Vector3(0, y*Time.deltaTime, 0);
+        Vector3 revolution = new Vector3(0, around * Time.deltaTime, 0);
+        transform.Rotate(angle);
+        transform.RotateAround(EarthPos.transform.position, revolution, around);
+    }
+}
